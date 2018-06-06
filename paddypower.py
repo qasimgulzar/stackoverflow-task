135550ceebc7f9516eed16532368c271baaa040e
qasimkhokhar <qasim.gulzar52@gmail.com>
2018-06-07 02:29:43 +0500

    [INIT]

---
+++ paddypower.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome("chromedriver")
+BASE_URL = "https://www.paddypower.com/"
+driver.get(BASE_URL)
+
+game = 'football'
+category = "World Cup 2018".lower()
+menu__sections = WebDriverWait(driver, 20).until(
+    EC.presence_of_all_elements_located(
+        (By.CLASS_NAME, "menu__section"))
+)
+
+for menu__section in menu__sections:
+    menu__items = WebDriverWait(menu__section, 20).until(
+        EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, "menu__item"))
+    )
+    for menu__item in menu__items:
+        if menu__item.text.lower() == game:
+            menu__item.click()
+
+            ribbon__contents = WebDriverWait(driver, 20).until(
+                EC.presence_of_all_elements_located(
+                    (By.CLASS_NAME, "ribbon__content"))
+            )
+            for ribbon__content in ribbon__contents:
+                ribbon__items = WebDriverWait(driver, 20).until(
+                    EC.presence_of_all_elements_located(
+                        (By.CLASS_NAME, "ribbon__item"))
+                )
+
+                for ribbon__item in ribbon__items:
+                    if ribbon__item.text.lower() == category:
+                        print(ribbon__item.text)
+                        ribbon__item.click()
+                        break
